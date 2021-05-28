02c9028305203da283537a663a4465fe7aad96c0
windy <frcwindy@gmail.com>
2021-05-28 18:45:58 +0800

    添加加密类容

--- app/src/main/java/com/rong/cheng/storage/ExternalDedicatedStorageSpaceActivity.kt
+++ app/src/main/java/com/rong/cheng/storage/ExternalDedicatedStorageSpaceActivity.kt
@@ -6,8 +6,15 @@ import android.util.Log
 import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import androidx.security.crypto.EncryptedFile
+import androidx.security.crypto.MasterKey
 import com.rong.cheng.storage.databinding.ActivityExternalDedicatedStorageBinding
+import java.io.ByteArrayOutputStream
 import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.lang.Exception
+import java.nio.charset.StandardCharsets
 
 /**
  * 外部存储空间
@@ -25,6 +32,8 @@ class ExternalDedicatedStorageSpaceActivity : AppCompatActivity(), View.OnClickL
         mBinding.getExternalStoragePath.setOnClickListener(this)
         mBinding.createPersistentFile.setOnClickListener(this)
         mBinding.createCacheFile.setOnClickListener(this)
+        mBinding.writeEncryptedWords.setOnClickListener(this)
+        mBinding.readEncryptedWords.setOnClickListener(this)
 
     }
 
@@ -34,8 +43,12 @@ class ExternalDedicatedStorageSpaceActivity : AppCompatActivity(), View.OnClickL
             mBinding.getExternalStoragePath -> getExternalStoragePath()
             mBinding.createPersistentFile -> createPersistentFile()
             mBinding.createCacheFile -> createCacheFile()
+            mBinding.writeEncryptedWords -> writeEncryptedWords()
+            mBinding.readEncryptedWords -> readEncryptedWords()
         }
     }
+
+
 
     private fun createCacheFile() {
         val cacheFile = File(externalCacheDir, "cache_file.txt")
@@ -64,6 +77,12 @@ class ExternalDedicatedStorageSpaceActivity : AppCompatActivity(), View.OnClickL
          */
         val persistentFile = File(this.getExternalFilesDir(null), "persistent_file.txt")
         if (persistentFile.exists()) {
+            FileOutputStream(persistentFile).use {
+                it.write("这是一个持久文件".toByteArray())
+            }
+//            openFileOutput(persistentFile.path, MODE_PRIVATE).use {
+//
+//            }
             showContent("创建持久文件成功，path ： ${persistentFile.path}")
         } else {
             persistentFile.createNewFile()
@@ -71,6 +90,75 @@ class ExternalDedicatedStorageSpaceActivity : AppCompatActivity(), View.OnClickL
         }
         //可以创造更细分的目录，比如图片。视频
 //        val albumFile =StorageQueryUtil.getAppSpecificAlbumStorageDir(this,"albumFile")
+    }
+
+    /**
+     * 写入加密文字
+     */
+    private fun writeEncryptedWords() {
+
+        val mainKey = MasterKey.Builder(applicationContext)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
+
+        val fileToWrite = "my_sensitive_data.txt"
+
+        val encryptedFile = EncryptedFile.Builder(
+            applicationContext,
+            File(getExternalFilesDir(null), fileToWrite),
+            mainKey,
+            EncryptedFile.FileEncryptionScheme.AES256_GCM_HKDF_4KB
+        ).build()
+
+        val fileContent = "MY SUPER-SECRET INFORMATION"
+            .toByteArray(StandardCharsets.UTF_8)
+
+        try {
+            encryptedFile.openFileOutput().apply {
+                write(fileContent)
+                flush()
+                close()
+            }
+            showContent("写入成功 ： 内容是：\n $fileContent")
+        }catch (e:Exception){
+            showContent("已经写入过了，不需要重复写入")
+        }
+
+
+
+
+    }
+
+    private fun readEncryptedWords() {
+        val fileToWrite = "my_sensitive_data.txt"
+        val sb = StringBuffer()
+        sb.append("未解密前读取到的文本： \n")
+        FileInputStream(File(getExternalFilesDir(null), fileToWrite)).bufferedReader().useLines {lines->
+            for (line in lines) {
+                sb.append(line)
+            }
+
+        }
+        val mainKey = MasterKey.Builder(this)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
+        val encryptedFile = EncryptedFile.Builder(
+            this,
+            File(getExternalFilesDir(null), fileToWrite),
+            mainKey,
+            EncryptedFile.FileEncryptionScheme.AES256_GCM_HKDF_4KB
+        ).build()
+        val inputStream = encryptedFile.openFileInput()
+        sb.append("\n")
+        sb.append("\n")
+        sb.append("\n")
+        sb.append("解密后的文本： \n")
+        inputStream.bufferedReader().useLines {
+            for (s in it) {
+                sb.append(s)
+            }
+        }
+        showContent(sb.toString())
     }
 
     private fun getExternalStoragePath() {
